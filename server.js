3028040e8882d54e6ecbb516377c4c27e6c12700
Sunny <sunnyinvadeher@gmail.com>
2018-09-24 10:08:38 -0700

    Fixed server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const fs = require('fs');
 const path = require('path');
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const twitter = require('twitter');
 const twitterConfig = require('./secret/twitter');
