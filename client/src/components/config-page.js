d840c0356e3890229f803606efc6c14b93ae6d3c
Sunny <sunnyinvadeher@gmail.com>
2018-09-24 09:59:35 -0700

    Prepared to deploy

---
+++ client/src/components/config-page.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+
+class ConfigPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            type: 0,
+            normalBossList: [],
+            hlBossList: [],
+            primarchBossList: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('/normalRaidBoss')
+            .then(res => res.json())
+            .then(result => {
+                var data = JSON.parse(result);
+                var arr = [];
+                data.forEach(el => {
+                    // this.setState({normalBossList: this.state.normalBossList.concat(el)});
+                    arr.push(el);
+                });
+                this.setState({normalBossList: arr});
+            }).catch(()=>{
+                console.log("Promise Rejected");
+            });
+        
+        fetch('/hlRaidBoss')
+            .then(res => res.json())
+            .then(result => {
+                var data = JSON.parse(result);
+                var arr = [];
+                data.forEach(el => {
+                    // this.setState({normalBossList: this.state.normalBossList.concat(el)});
+                    arr.push(el);
+                });
+                this.setState({hlBossList: arr});
+            }).catch((e)=>{
+                console.log(e);
+            });
+
+        fetch('/primarchRaidBoss')
+            .then(res => res.json())
+            .then(result => {
+                var data = JSON.parse(result);
+                var arr = [];
+                data.forEach(el => {
+                    // this.setState({normalBossList: this.state.normalBossList.concat(el)});
+                    arr.push(el);
+                });
+                this.setState({primarchBossList: arr});
+            }).catch((e)=>{
+                console.log(e);
+            });
+    }
+
+    render() {
+        const bossList = this.state.normalBossList.map((boss) => {
+            // console.log(boss);
+            return (
+                <li key={boss.title} onClick={() => 
+                    this.props.add(boss.title, boss.subtitle)} className="gbfrf-js-bossSelect gbfrf-follow__boss-box mdl-list__item mdl-list__item--two-line" >
+                    <span className="mdl-list__item-primary-content">
+                        <span>
+                            {boss.title}
+                        </span>
+                        <span className="gbfrf-follow__boss-box-subtitle mdl-list__item-sub-title">
+                            {boss.subtitle}
+                        </span>
+                    </span>
+                </li>
+            );
+        });
+        
+        return(
+            <dialog className="mdl-dialog gbfrf-dialog" open={this.props.showDialog ? 'open': ''}>
+                <div className="mdl-layout__container">
+                    <div className="gbfrf-main-dialog gbfrf-dialog__container mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-tabs is-upgraded is-small-screen">
+                        <header className="mdl-layout__header is-casting-shadow">
+                            <div className="mdl-layout__header-row gbfrf-column__header-row gbfrf-dialog__tab-bar">
+                                <div className="gbfrf-dialog__tab-bar-item mdl-layout__tab is-active">
+                                    Normal
+                                </div>
+                                <div className="gbfrf-dialog__tab-bar-item mdl-layout__tab">
+                                    High Level
+                                </div>
+                                <div className="gbfrf-dialog__tab-bar-item mdl-layout__tab">
+                                    Event/GuildWar
+                                </div>
+                                {/* <div className="mdl-layout-spacer"></div> */}
+                                <div className="mdl-button mdl-js-button mdl-button--icon material-icons">
+                                    <i className="material-icons" onClick={this.props.hide}>clear</i>
+                                </div>
+                            </div>
+                        </header>
+                        <section className="gbfrf-dialog__content mdl-layout__tab-panel is-active">
+                            <ul className="mdl-list" >
+                                {bossList}
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+            </dialog>
+        );
+    }
+}
+
+export default ConfigPage;
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const fs = require('fs');
+const path = require('path');
+const port = 3001;
+
+const twitter = require('twitter');
+const twitterConfig = require('./secret/twitter');
+const tweets = new twitter(twitterConfig.config);
+
+// console.log('** DEV **');
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/normalRaidBoss', (req, res)=>{
+    fs.readFile(`assets/normalRaidBoss.txt`, 'utf-8', (err, data) => {
+        if(err){
+            throw err;
+        }
+        res.json(data);
+    })
+});
+
+app.get('/hlRaidBoss', (req, res)=>{
+    fs.readFile(`assets/hlRaidBoss.txt`, 'utf-8', (err, data) => {
+        if(err){
+            throw err;
+        }
+        res.json(data);
+    })
+});
+
+app.get('/primarchRaidBoss', (req, res)=>{
+    fs.readFile(`assets/primarchRaidBoss.txt`, 'utf-8', (err, data) => {
+        if(err){
+            throw err;
+        }
+        res.json(data);
+    })
+});
+
+// start up the node server
+server.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
+
+// create a socket.io connection with the client
+io.on('connection', (socket)=>{
+    console.log(`User connected. Socket id ${socket.id}`);
+    socket.on('disconnect',()=>{
+        console.log(`User disconnected. Socket id ${socket.id}`);
+    });
+});
+
+// listen to the twitter stream and tweet comes in send it to the client real time
+tweets.stream('statuses/filter', { track: "参加者募集！,I need backup!" }, (stream) => {
+    stream.on('data', (data) => {
+        io.sockets.emit('tweet', data);
+        // console.log(data.text);
+    });
+});
